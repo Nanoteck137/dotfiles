5a63e61ee5d6862735c9fc73fbdd502edd28ea74
Patrik M. Rosenström <patrik.millvik@gmail.com>
2022-11-10 12:17:34 +0100

    Removed plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -7,21 +7,12 @@ return require('packer').startup(function()
     use 'joshdick/onedark.vim'
     use 'folke/tokyonight.nvim'
 
+    use 'nvim-treesitter/nvim-treesitter'
+
     use 'nvim-lua/popup.nvim'
     use 'nvim-lua/plenary.nvim'
     use 'nvim-telescope/telescope.nvim'
     use 'nvim-telescope/telescope-fzy-native.nvim'
-
-    use 'nvim-treesitter/nvim-treesitter'
-
-    use 'neovim/nvim-lspconfig'
-
-    use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-
-    use 'numToStr/Comment.nvim'
 
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
@@ -30,25 +21,24 @@ return require('packer').startup(function()
     use 'hrsh7th/nvim-cmp'
 
     use 'saadparwaiz1/cmp_luasnip'
-
     use 'L3MON4D3/LuaSnip'
+
+    use 'rcarriga/nvim-notify'
+
+    use 'neovim/nvim-lspconfig'
+
+    use 'kyazdani42/nvim-web-devicons'
+    use 'nvim-lualine/lualine.nvim'
+
+    use 'numToStr/Comment.nvim'
 
     use 'rhysd/committia.vim'
     use 'beauwilliams/focus.nvim'
 
-    use({
+    use {
         "iamcco/markdown-preview.nvim",
         run = function() vim.fn["mkdp#util#install"]() end,
-    })
-
-    use 'kyazdani42/nvim-web-devicons'
-
-    use {
-        "X3eRo0/dired.nvim",
-        requires = "MunifTanjim/nui.nvim",
     }
-
-    use 'rcarriga/nvim-notify'
 
     use "~/plugins/sobble.nvim"
 end)
