e6d50c084cdba7660b0c6675a4405593066da4e3
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-09-22 10:38:36 +0200

    Removed boot device

--- hosts/raichu/hardware-configuration.nix
+++ hosts/raichu/hardware-configuration.nix
@@ -13,11 +13,6 @@
     fsType = "ext4";
   };
 
-  fileSystems."/boot" = { 
-    device = "/dev/disk/by-label/boot";
-    fsType = "vfat";
-  };
-
   swapDevices = [ 
     { device = "/dev/disk/by-label/swap"; }
   ];
