8e52f1aa5e4ccf70f3ecca1a9bc025c8ed7a941f
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-01-22 08:31:00 +0100

    Update

--- flake.nix
+++ flake.nix
@@ -48,6 +48,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.nanoteck137 = import ./hosts/krokorok/home.nix;
           }
         ];
