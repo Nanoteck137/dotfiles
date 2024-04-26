bd967c944223656297e308029de9b0ecef9728b5
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-04-26 14:47:23 +0200

    Fixed issue

--- flake.nix
+++ flake.nix
@@ -63,6 +63,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
             home-manager.users.nanoteck137 = import ./hosts/pichu/home.nix;
           }
         ];
