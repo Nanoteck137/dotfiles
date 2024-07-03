4dfae8f79cb0f81ef825d4959cb96a235826491b
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-07-03 15:28:32 +0200

    Disable pipewire

--- hosts/klink/configuration.nix
+++ hosts/klink/configuration.nix
@@ -50,12 +50,12 @@ in {
 
   sound.enable = true;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
+  # services.pipewire = {
+  #   enable = true;
+  #   alsa.enable = true;
+  #   alsa.support32Bit = true;
+  #   pulse.enable = true;
+  # };
 
   users.users.nanoteck137 = {
     isNormalUser = true;
