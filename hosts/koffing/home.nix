8f75041fd74432b9e71faa6169eec3864ad96918
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-10-19 21:44:12 +0200

    Testing

--- hosts/koffing/home.nix
+++ hosts/koffing/home.nix
@@ -29,19 +29,19 @@
   # qt.style.name = "adwaita-dark";
   # qt.style.package = pkgs.adwaita-qt;
 
-  gtk.enable = true;
-
-  gtk.cursorTheme.package = pkgs.simp1e-cursors;
-  gtk.cursorTheme.name = "Simp1e-Tokyo-Night-Storm";
-
-  gtk.theme.package = pkgs.tokyo-night-gtk;
-  gtk.theme.name = "Tokyonight-Storm-BL";
+  # gtk.enable = true;
+  #
+  # gtk.cursorTheme.package = pkgs.simp1e-cursors;
+  # gtk.cursorTheme.name = "Simp1e-Tokyo-Night-Storm";
+  #
+  # gtk.theme.package = pkgs.tokyo-night-gtk;
+  # gtk.theme.name = "Tokyonight-Storm-BL";
 
   # gtk.iconTheme.package = pkgs.papirus-icon-theme;
   # gtk.iconTheme.name = "Papirus-Dark";
 
-  gtk.iconTheme.package = pkgs.dracula-icon-theme;
-  gtk.iconTheme.name = "Dracula";
+  # gtk.iconTheme.package = pkgs.dracula-icon-theme;
+  # gtk.iconTheme.name = "Dracula";
 
   home.stateVersion = "23.05";
 }
