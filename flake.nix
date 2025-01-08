b9bd2245e7a03eb5eedcfeeb64f6b117dcfefaea
Patrik M. Rosenström <patrik.millvik@gmail.com>
2025-01-08 03:33:17 +0100

    Working on updating the nix config

--- flake.nix
+++ flake.nix
@@ -23,20 +23,11 @@
     sewaddle.url = "github:nanoteck137/sewaddle";
     sewaddle.inputs.nixpkgs.follows = "nixpkgs";
 
-    sewaddle-web.url = "github:nanoteck137/sewaddle-web/v0.9.1-3";
-    sewaddle-web.inputs.nixpkgs.follows = "nixpkgs";
-
     dwebble.url = "github:nanoteck137/dwebble";
     dwebble.inputs.nixpkgs.follows = "nixpkgs";
     
-    dwebble-frontend.url = "github:nanoteck137/dwebble-frontend/v0.15.5-3";
-    dwebble-frontend.inputs.nixpkgs.follows = "nixpkgs";
-
     kricketune.url = "github:nanoteck137/kricketune";
     # kricketune.inputs.nixpkgs.follows = "nixpkgs";
-
-    crustle.url = "github:nanoteck137/crustle";
-    crustle.inputs.nixpkgs.follows = "nixpkgs";
 
     customcaddy.url = "github:nanoteck137/customcaddy";
     # customcaddy.inputs.nixpkgs.follows = "nixpkgs";
@@ -45,105 +36,111 @@
   outputs = { self, nixpkgs, nix-darwin, home-manager, ... }@inputs: 
     let 
     in {
-      nixosConfigurations.krokorok = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit self inputs; };
-        modules = [ 
-          ./hosts/krokorok/configuration.nix 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit self inputs; };
-            home-manager.users.nanoteck137 = import ./hosts/krokorok/home.nix;
-          }
-        ];
+      nixosConfigurations = let 
+        buildSystem = { name, system ? "x86_64-linux" }: nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit self inputs; };
+          modules = [ 
+            ./hosts/${name}/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit self inputs; };
+              home-manager.users.nanoteck137 = import ./hosts/${name}/home.nix;
+            }
+          ];
+        };
+      in{
+        krokorok = buildSystem {
+          name = "krokorok";
+        };
+
+        pichu = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [ 
+            ./hosts/pichu/configuration.nix 
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit inputs; };
+              home-manager.users.nanoteck137 = import ./hosts/pichu/home.nix;
+            }
+          ];
+        };
+
+        raichu = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [ 
+            ./hosts/raichu/configuration.nix 
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit inputs; };
+              home-manager.users.nanoteck137 = import ./hosts/raichu/home.nix;
+            }
+          ];
+        };
+
+        klink = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [ 
+            ./hosts/klink/configuration.nix 
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit self inputs; };
+              home-manager.users.nanoteck137 = import ./hosts/klink/home.nix;
+            }
+          ];
+        };
+
+        koffing = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [ 
+            ./hosts/koffing/configuration.nix 
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit self inputs; };
+              home-manager.users.nanoteck137 = import ./hosts/koffing/home.nix;
+            }
+          ];
+        };
+
+        testvm = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [ 
+            ./hosts/testvm/configuration.nix 
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit self inputs; };
+              home-manager.users.nanoteck137 = import ./hosts/testvm/home.nix;
+            }
+          ];
+        };
+
+        iso = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [ 
+            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+            ./hosts/iso/configuration.nix 
+          ];
+        };
       };
 
-      nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [ 
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-          ./hosts/iso/configuration.nix 
-        ];
-      };
-
-      nixosConfigurations.pichu = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [ 
-          ./hosts/pichu/configuration.nix 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-            home-manager.users.nanoteck137 = import ./hosts/pichu/home.nix;
-          }
-        ];
-      };
-
-      nixosConfigurations.raichu = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [ 
-          ./hosts/raichu/configuration.nix 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs; };
-            home-manager.users.nanoteck137 = import ./hosts/raichu/home.nix;
-          }
-        ];
-      };
-
-      nixosConfigurations.klink = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [ 
-          ./hosts/klink/configuration.nix 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit self inputs; };
-            home-manager.users.nanoteck137 = import ./hosts/klink/home.nix;
-          }
-        ];
-      };
-
-      nixosConfigurations.koffing = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [ 
-          ./hosts/koffing/configuration.nix 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit self inputs; };
-            home-manager.users.nanoteck137 = import ./hosts/koffing/home.nix;
-          }
-        ];
-      };
-
-      nixosConfigurations.testvm = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [ 
-          ./hosts/testvm/configuration.nix 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit self inputs; };
-            home-manager.users.nanoteck137 = import ./hosts/testvm/home.nix;
-          }
-        ];
-      };
-        
       homeConfigurations.test = home-manager.lib.homeManagerConfiguration {
         pkgs = import nixpkgs { system = "x86_64-linux"; };
         extraSpecialArgs = { inherit self inputs; };
