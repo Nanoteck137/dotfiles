4ad8db843cda1eb2712cba94ecea581337341fbb
Patrik M. Rosenström <patrik.millvik@gmail.com>
2025-09-13 16:37:32 +0200

    Fixed modules

--- homeManagerModules/zsh.nix
+++ homeManagerModules/zsh.nix
@@ -1,22 +1,54 @@
-{ self, config, pkgs, lib, ... }: 
+{ config, pkgs, lib, ... }: 
 with lib; let 
   cfg = config.nano.home.zsh;
 in {
   options = {
-    nano.home.alacritty = {
-      enable = lib.mkEnableOption "enable alacritty";
+    nano.home.zsh = {
+      enable = lib.mkEnableOption "enable zsh";
     };
   };
 
   config = lib.mkIf cfg.enable {
-    programs.alacritty = {
-      enable = true;
-    }; 
-
-    home.packages = [
-      pkgs.nerd-fonts.noto
+    home.packages = with pkgs; [
+      (pkgs.writeShellScriptBin "dev-shell" ''
+       export REALSHELL=$SHELL
+       nix develop --command $SHELL
+       '')
     ];
 
-    xdg.configFile.alacritty.source = "${self}/configs/alacritty";
+    programs.zsh = {
+      enable = true;
+
+      oh-my-zsh = {
+        enable = true;
+        theme = "af-magic";
+      };
+
+      shellAliases = {
+        "lg" = "${pkgs.lazygit}/bin/lazygit";
+        "nvim-dev" = "NVIM_APPNAME=nvim-dev nvim";
+      };
+
+      initExtra = if pkgs.stdenv.isDarwin then ''
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+
+        ${pkgs.any-nix-shell}/bin/any-nix-shell zsh --info-right | source /dev/stdin
+
+        export PATH=~/.npm-global/bin:$PATH
+        export PATH=~/opt/flutter/bin:$PATH
+
+        if [[ -n $REALSHELL ]]; then
+          export SHELL=$REALSHELL
+            fi
+
+        '' 
+        else "
+          ${pkgs.any-nix-shell}/bin/any-nix-shell zsh --info-right | source /dev/stdin
+
+          if [[ -n $REALSHELL ]]; then
+            export SHELL=$REALSHELL
+          fi
+        ";
+    };
   };
 }
