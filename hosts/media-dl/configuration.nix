cdacfafb5f4772e3cec08c9b3169767825e6336a
Patrik M. Rosenström <patrik.millvik@gmail.com>
2025-10-16 03:34:54 +0200

    Added klink media

--- hosts/media-dl/configuration.nix
+++ hosts/media-dl/configuration.nix
@@ -31,6 +31,16 @@ in {
     home.stateVersion = "23.05";
   };
 
+  fileSystems."/mnt/klink-media" = {
+      device = "//10.28.28.9/media2";
+      fsType = "cifs";
+      options = let
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        user = "uid=1000,gid=100";
+
+      in ["${automount_opts},${user},credentials=/etc/nixos/smb-secrets"];
+  };
+
   services.qbittorrent = {
     enable = true;
     openFirewall = true;
