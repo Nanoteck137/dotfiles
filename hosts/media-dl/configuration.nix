126ef8beb4366fbb4ae04ee4f4c65b0614db1daf
Patrik M. Rosenström <patrik.millvik@gmail.com>
2025-10-16 03:53:11 +0200

    More testing

--- hosts/media-dl/configuration.nix
+++ hosts/media-dl/configuration.nix
@@ -38,7 +38,7 @@ in {
       fsType = "cifs";
       options = let
         automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-        user = "uid=1000,gid=${config.users.groups.media.gid}";
+        user = "uid=1000,gid=${config.ids.gids.media}";
 
       in ["${automount_opts},${user},credentials=/etc/nixos/smb-secrets"];
   };
