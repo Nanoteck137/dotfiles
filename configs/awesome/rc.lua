d8546247cbdb062951bc6b0475ba8db840bca9ca
Patrik M. Rosenström <patrik.millvik@gmail.com>
2025-12-12 03:38:54 +0100

    Used alacritty as the default terminal for awesome

--- configs/awesome/rc.lua
+++ configs/awesome/rc.lua
@@ -56,7 +56,7 @@ local home = os.getenv("HOME")
 beautiful.init(gears.filesystem.get_configuration_dir() .. "theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-local terminal = "kitty"
+local terminal = "alacritty"
 local editor = os.getenv("EDITOR") or "nvim"
 local editor_cmd = terminal .. " -e " .. editor
 
